f3a971505fac41d9825c3e25f46a852dfd812d8c
Zander Mackie <zmackie@gmail.com>
2016-11-18 16:18:48 -0500

    Getting factors

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,7 +1,22 @@
 package raindrops
 
+import (
+	"fmt"
+	"math"
+)
+
 const testVersion = 2
 
-func Convert(int) string
+func Convert(n int) string {
+	limit := int(math.Sqrt(float64(n)))
+	factors := []int{}
+	for i := 1; i < limit; i++ {
+		if n%i == 0 {
+			factors = append(factors, i)
+		}
+	}
+	fmt.Println(factors)
+	return "test"
+}
 
 // The test program has a benchmark too.  How fast does your Convert convert?
