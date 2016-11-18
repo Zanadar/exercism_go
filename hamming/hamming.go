599b0ba058e36188e648091a1ddbe572e700c8fe
Zander Mackie <zmackie@gmail.com>
2016-11-18 16:06:55 -0500

    Hamming done

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,7 +1,20 @@
 package hamming
 
+import "errors"
+
 const testVersion = 5
 
+//Distance returns how many characters are different from their equivalent in the other string.
+//It expects strings of equal length.
 func Distance(a, b string) (int, error) {
-	// your code here!
+	if len(a) != len(b) {
+		return -1, errors.New("Strings must be of equal length.")
+	}
+	var diff int
+	for i := range a {
+		if a[i] != b[i] {
+			diff++
+		}
+	}
+	return diff, nil
 }
