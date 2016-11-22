31d402051eced81ee0c3dfb246a3ead2d1343843
Zander Mackie <zmackie@gmail.com>
2016-11-21 19:06:34 -0500

    cleanup

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,7 +1,6 @@
 package raindrops
 
 import (
-	"fmt"
 	"math"
 	"strconv"
 	"strings"
@@ -9,17 +8,16 @@ import (
 
 const testVersion = 2
 
+//Convert takes in integer and returns a rain patter string based on the factors of the integer.
+//
 func Convert(n int) string {
-	fmt.Println("************** new Call to convert n is: ", n)
 	limit := int(math.Sqrt(float64(n)) + 1)
 	factors := []int{}
 
 	for i := 1; i < limit; i++ {
 		if n%i == 0 {
-			fmt.Println("Factor added: ", i)
 			factors = append(factors, i)
 			if i != n/i {
-				fmt.Println("Factor added: ", n/i)
 				factors = append(factors, n/i)
 			}
 		}
@@ -27,27 +25,21 @@ func Convert(n int) string {
 
 	var rainStrings []string
 
-	for i, factor := range factors {
-		fmt.Println(i, factor, factors)
+	for _, factor := range factors {
 		switch factor {
 		case 3:
 			rainStrings = append(rainStrings, "Pling")
-			fmt.Println("3 factor", rainStrings)
 		case 5:
 			rainStrings = append(rainStrings, "Plang")
-			fmt.Println("5 factor", rainStrings)
 		case 7:
 			rainStrings = append(rainStrings, "Plong")
-			fmt.Println("7 factor", rainStrings)
 		default:
-			fmt.Println("Garbage")
 		}
 
 	}
 	if len(rainStrings) == 0 {
 		return strconv.Itoa(n)
 	}
-	fmt.Println(n, rainStrings)
 	return strings.Join(rainStrings, "")
 }
 
